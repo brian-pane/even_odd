546f52359e9e51be26056a861f90b32cef7554ad
Brian Pane <brianp+github@brianp.net>
2025-12-20 16:22:11 -0800

    Add custom, non-Rayon thread pool implementation

--- benches/even_odd_benchmark.rs
+++ benches/even_odd_benchmark.rs
@@ -1,5 +1,6 @@
 use std::hint::black_box;
 use criterion::{Criterion, criterion_group, criterion_main};
+use even_odd::{EvenOdd};
 
 fn max(c: &mut Criterion) {
     c.bench_function("is_even(u32::MAX)", |b| b.iter(|| even_odd::is_even(black_box(u32::MAX))));
@@ -7,10 +8,17 @@ fn max(c: &mut Criterion) {
 
 criterion_group!(single_threaded, max);
 
-fn max_parallel(c: &mut Criterion) {
-    c.bench_function("is_even_parallel(u32::MAX)", |b| b.iter(|| even_odd::is_even_parallel(black_box(u32::MAX))));
+fn max_rayon(c: &mut Criterion) {
+    c.bench_function("is_even_rayon(u32::MAX)", |b| b.iter(|| even_odd::is_even_rayon(black_box(u32::MAX))));
 }
 
-criterion_group!(multi_threaded, max_parallel);
+criterion_group!(rayon, max_rayon);
 
-criterion_main!(single_threaded, multi_threaded);
+fn max_threadpool(c: &mut Criterion) {
+    let even_odd = EvenOdd::new();
+    c.bench_function("is_even_threadpool(u32::MAX)", |b| b.iter(|| even_odd.is_even(black_box(u32::MAX))));
+}
+
+criterion_group!(threadpool, max_threadpool);
+
+criterion_main!(single_threaded, rayon, threadpool);
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
+use std::thread;
+use std::thread::JoinHandle;
 use rayon::prelude::*;
 
 pub fn is_even(num: u32) -> bool {
@@ -13,15 +16,14 @@ pub fn is_even(num: u32) -> bool {
     even
 }
 
-pub fn is_even_parallel(num: u32) -> bool {
-    // A block is a fixed, power-of-two size. A chunk consists of one or more blocks.
-    const LOG2_BLOCK_SIZE: usize = 12;
-    const BLOCK_SIZE: usize = 1 << LOG2_BLOCK_SIZE;
-    const TOTAL_BLOCKS: usize = 1 << (32 - LOG2_BLOCK_SIZE);
+const LOG2_BLOCK_SIZE: usize = 12;
 
-    let num_chunks = std::thread::available_parallelism().unwrap().get();
+const BLOCK_SIZE: usize = 1 << LOG2_BLOCK_SIZE;
+
+const TOTAL_BLOCKS: usize = 1 << (32 - LOG2_BLOCK_SIZE);
+
+fn partition_chunks(num_chunks: usize) -> Vec<(usize, usize)> {
     let num_chunks = core::cmp::min(num_chunks, TOTAL_BLOCKS);
-
     let mut chunks = Vec::with_capacity(num_chunks);
     let mut start_block: usize = 0;
     let blocks_per_chunk = TOTAL_BLOCKS / num_chunks;
@@ -34,7 +36,11 @@ pub fn is_even_parallel(num: u32) -> bool {
         chunks.push((start_block, num_blocks));
         start_block += num_blocks;
     }
+    chunks
+}
 
+pub fn is_even_rayon(num: u32) -> bool {
+    let chunks = partition_chunks(std::thread::available_parallelism().unwrap().get());
     let even = Arc::new(Mutex::new(false));
     let worker_even = Arc::clone(&even);
     chunks.par_iter().for_each(move |&(start_block, num_blocks)| {
@@ -54,6 +60,75 @@ pub fn is_even_parallel(num: u32) -> bool {
     even.lock().unwrap().clone()
 }
 
+pub struct EvenOdd {
+    workers: Vec<JoinHandle<()>>,
+    to_workers: Vec<mpsc::Sender<Option<Request>>>,
+}
+
+struct Request {
+    num: u32,
+    tx: mpsc::Sender<bool>,
+}
+
+impl EvenOdd {
+    pub fn new() -> Self {
+        let chunks = partition_chunks(std::thread::available_parallelism().unwrap().get());
+        let mut workers = Vec::with_capacity(chunks.len());
+        let mut to_workers = Vec::with_capacity(chunks.len());
+        for (start_block, num_blocks) in chunks {
+            let (tx, rx) = mpsc::channel::<Option<Request>>();
+            to_workers.push(tx);
+           workers.push(thread::spawn(move || {
+               loop {
+                   let received = rx.recv().unwrap();
+                   match received {
+                       None => break,
+                       Some(request) => {
+                           let mut local_even = false;
+                           let mut known_even = (start_block * BLOCK_SIZE) as u32;
+                           for _ in 0..BLOCK_SIZE * num_blocks / 2 {
+                               if known_even == request.num {
+                                   local_even = true;
+                               }
+                               known_even = known_even.wrapping_add(2);
+                           }
+                           request.tx.send(local_even).unwrap();
+                       }
+                   }
+               }
+           }));
+        }
+        Self {
+            workers,
+            to_workers,
+        }
+    }
+
+    pub fn is_even(&self, num: u32) -> bool {
+        let mut from_workers = Vec::with_capacity(self.to_workers.len());
+        for to_worker in &self.to_workers {
+            let (tx, rx) = mpsc::channel::<bool>();
+            from_workers.push(rx);
+            to_worker.send(Some(Request { num, tx})).unwrap();
+        }
+        let mut even = false;
+        for rx in from_workers {
+            let received = rx.recv().unwrap();
+            even |= received;
+        }
+        even
+    }
+}
+
+impl Drop for EvenOdd {
+    fn drop(&mut self) {
+        self.to_workers.iter().for_each(|tx| {
+           tx.send(None).unwrap();
+        });
+        self.workers.drain(..).for_each(|w| w.join().unwrap());
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use core::hint::black_box;
@@ -67,9 +142,17 @@ mod tests {
     }
 
     #[test]
-    fn multi_threaded() {
+    fn rayon() {
         for (num, expected) in [(0, true), (1, false), (2, true), (9_999_999, false), (u32::MAX, false)] {
-            assert_eq!(is_even_parallel(black_box(num)), expected);
+            assert_eq!(is_even_rayon(black_box(num)), expected);
+        }
+    }
+
+    #[test]
+    fn even_odd() {
+        let even_odd = EvenOdd::new();
+        for (num, expected) in [(0, true), (1, false), (2, true), (9_999_999, false), (u32::MAX, false)] {
+            assert_eq!(even_odd.is_even(black_box(num)), expected);
         }
     }
 }
